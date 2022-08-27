3f46489cc24b30b9f8ea4243d08cdd7aaf6188bc
Adam Attsaev <adam2418@mail.ru>
2022-08-27 12:13:19 +0300

    Проверка

---
+++ index.js
@@ -0,0 +1,236 @@
+/*let n1 = 2+2
+let n2 = 2*4
+let n3 = 10/2
+let n4 = 4-1
+let n5 = 2**3
+console(n1+n2+n3+n4+n5)
+n1 = 10+2
+console(n1+n2+n3+n4+n5)*/
+/*age = 32
+console(age)
+age = 32 + 1
+console("Спустя год age будет", age)*/
+/*let num1 = 2
+let num2 = 4
+let num3 = 6
+let num4 = 8
+let num5 = 10
+let str1 = "Да"
+let str2 = "Нет"
+let str3 = "Может быть"
+let str4 = "Мало"
+let str5 = "Много"
+console(num1, num2, num3, num4, num5, str1, str2, str3, str4, str5 )  
+num1 = 3
+num2 = 6
+num3 = 9
+str1 = "Легко"
+str2 = "Средне"    
+str3 = "Сложно"
+console(num1, num2, num3, str1, str2, str3)*/
+
+// let names = ["Adam", "Maga", "Sul", "Ahmed", "Jaba"]
+/*let nameAge = ["Adam", 18, "Maga", 20, "Sul", 22, "Ahmed", 24, "Jaba", 26]
+console.log("Имя:", nameAge[0], ",", "Возраст:", nameAge[1])
+console.log("Имя:", nameAge[2], ",", "Возраст:", nameAge[3])
+console.log("Имя:", nameAge[4], ",", "Возраст:", nameAge[5])
+console.log("Имя:", nameAge[6], ",", "Возраст:", nameAge[7])
+console.log("Имя:", nameAge[8], ",", "Возраст:", nameAge[9])*/
+/*let myName = "Adam"
+let age = 22
+let myFriendsName = "Maga"
+let car = "BMV x1 2018y"
+let myCar = car
+console.log("Моя машина", myCar, "такая же как у ",myFriendsName, ". Его машина", car, "."  ) */
+/*let names = [["Movsar", 32], ["Milana", 39], ["Magomed", 22], ["Adam", 22], ["Suliman", 21]]
+console.log("My teacher name is " + names[0][0] + ". He is " + names[0][1])
+console.log("His sisters name is " + names[1][0] + ". She is " + names[1][1])
+console.log("My best friends name is " + names[2][0] + ". He is " + names[2][1])
+console.log("My name is " + names[3][0] + ". I am " + names[3][1])
+console.log("My good friends name is " + names[4][0], ". He is ", names[4][1])*/
+//let index = 0
+//let religion = "ISLAM"
+//let str = "Muslim"
+/*console.log(religion[index])
+console.log(religion[index + 1])
+console.log(religion[index + 2])
+console.log(religion[index + 3])
+console.log(religion[index + 4])*/
+/*console.log(str[0])
+console.log(str[1])
+console.log(str[2])
+console.log(str[3])
+console.log(str[4])
+console.log(str[5])
+console.log(str[6])
+console.log(str[7])
+console.log(str[8])
+console.log(str[9])
+console.log(str[10])
+console.log(str[11])
+console.log(str[12])
+console.log(str[13])
+console.log(str[14])
+console.log(str[15])*/
+/*let str = "This is very long string. I dont want to console it"
+let arr = [1,2,3,4,5]
+console.log("Длина строки:", str.length )
+console.log("Длина массива arr:",arr.length)
+console.log(3<5)
+console.log(3>5)
+console.log(3<=3)
+console.log(3>=2)
+console.log("ab" === "ab")
+console.log("ad" !== "ab")
+console.log("ad" === "ad")*/
+/*let age = 20
+let myName = "Lua"
+let horror = "Ужас"
+let actionMovie = "Боевик"
+let cartoon = "Мультик"
+if (age >= 18) {
+    console.log("Вам ", age, "лет, ", "поэтому смотреть можно ", horror, "жанр")
+}
+else if (age>12) {
+    console.log("Вам ", age, "лет, ", "поэтому смотреть можно ", actionMovie, "жанр")
+}
+else {
+    console.log("Вам ", age, "лет, ", "поэтому смотреть можно ", cartoon, "жанр")
+}*/
+/*let names = "Chechnya"
+let index = 0
+console.log(names[index++])
+console.log(names[index++])
+console.log(names[index++])
+console.log(names[index++])
+console.log(names[index++])
+console.log(names[index++])
+console.log(names[index++])
+console.log(names[index++])
+console.log(index)*/
+/*let s = "Chechnya"
+for (let i = 0; i < s.length; i++ ) {
+    console.log(s[i])*/
+//for (let i = 21; i < 122; i+=2) {
+//    console.log(i)
+//}
+//for (let i = 2; i < 37; i++) {
+//    if (i % 2 === 0) {
+//        console.log(i)
+//    }
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*for(let r = 2; r < 38; r++) {
+    if (r % 2 === 1) {
+        console.log(r)
+    }
+}*/
+/*for (let i = 0; i < 100; i++) {
+    if (i % 2 === 0) {
+        console.log("Чётное")
+    }
+    else if (i % 2 === 1) {
+        console.log("Нечётное")
+    }
+    
+} */
+// && И || ИЛИ
+/*let names = ["Kazbek", "Lee", "Movsar", "Maga", "Suliman"]
+let i = 0*/
+//for ( i = 0; i<names.length; i++) {
+//    if (i === 0) {
+//        console.log(names[i] ,"О! Я выиграл");
+//    break }
+//    else if (i > 0)  {
+//        console.log(names[i] ,"Печально. Меня нет в списке")
+//    }
+//}
+/*for (i = 0; i <names.length; i++) {
+    if(names[i] === "Movsar") {
+        console.log("I won")
+    break }
+    else if(i === names.length - 1) {
+        if(names[i] !== "Movsar") {
+            console.log("I am not in the list")
+        }
+    }
+}*/
+/*for (i = 0; i<names.length; i++) {
+    let currentName = names[i]
+    let firstLetter = currentName[0]
+    let lastLetter = currentName[currentName.length - 1]
+    let nameLength = currentName.length
+    console.log(currentName ,firstLetter, lastLetter, nameLength)
+}*/
+
+let names = ['Adam', 'Lee', 'Movsar', 'Kazbek', 'Alibek']
+
+/*for (let i = 0; i < names.length; i++) {
+    let currentName = names[i]
+    let nameLength = currentName.length
+    let firstLetter = currentName[0]
+    let lastLetter = currentName[nameLength - 1] 
+}*/
+
+let upperCaseNames = []
+for (let i = 0; i < names.length; i++) {
+    let currName = names[i].toUpperCase()
+    upperCaseNames.push(currName)
+    //console.log(upperCaseNames)
+    
+}
+let lowerCaseNames = []
+for (let i = 0; i < names.length; i++) {
+    currName = names[i].toLowerCase()
+    lowerCaseNames.push(currName)
+    //console.log(lowerCaseNames)
+}
+let mixCaseLetters = []
+for (i = 0; i < names.length; i++) {
+    if (i % 2 === 0) {
+      currName = names[i].toUpperCase()
+        mixCaseLetters.push(currName)
+       // console.log(mixCaseLetters)
+    }
+    else if (i % 2 === 1) {
+      currName = names[i].toLowerCase()
+        mixCaseLetters.push(currName)
+       // console.log(mixCaseLetters)
+    }
+}
+console.log(mixCaseLetters)
+let diffNamesLenght =[]
+for (i = 0; i < names.length; i++) {
+    if (names[i].length % 2 === 1) {
+
+        diffNamesLenght.push(names[i])
+    }
+}
+console.log(diffNamesLenght)
+console.log('Доброго вам вечерочка'.substring(0, 21))
